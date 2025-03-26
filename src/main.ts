405f0fd224b2e03e5a3441737b2c6e40842dedb9
LokmanCode <lokman@approva.co>
2025-03-26 11:34:44 +0100

    initail deploy

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('Starting InstaFi APIs ...');
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port);
